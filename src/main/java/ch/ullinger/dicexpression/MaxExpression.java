1298baad76ad107200249bb80d0cfb1c3fa7a4bf
Max Ullinger <max.ullinger@1und1.de>
2016-02-19 11:06:18 +0100

    Added initial Min/Max Expressions

---
+++ src/main/java/ch/ullinger/dicexpression/MaxExpression.java
@@ -0,0 +1,34 @@
+package ch.ullinger.dicexpression;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxExpression implements Expression {
+
+    private List<Expression> expressions = new ArrayList<Expression>();
+
+    public MaxExpression(final Expression... expressions) {
+        for (Expression expression : expressions) {
+            this.expressions.add(expression);
+        }
+    }
+
+    public List<Expression> getExpressions() {
+        return expressions;
+    }
+
+    @Override
+    public int evaluate() {
+        int result = expressions.get(0).evaluate();
+
+        for (Expression expression : expressions) {
+            int evaluation = expression.evaluate();
+            if (evaluation > result) {
+                result = evaluation;
+            }
+        }
+
+        return result;
+    }
+
+}
